18a3a90cb7579a31293c62f2576475476ed34ff6
SAIRISAN123 <sairisan123@gmail.com.com>
2024-08-31 19:34:27 +0530

    added jobs

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "title": "Software Engineer",
+        "salary": "Rs. 1,000,000",
+        "location": "Mountain View, CA",
+        "description": "Work with a team of engineers to build software products for Google."
+    },
+    {
+        "title": "Network Engineer",
+        "salary": "Rs. 800,000",
+        "location": "Menlo Park, CA",
+        "description": "Lead a team of engineers to build network for Facebook"
+    },
+    {
+        "title": "Data Scientist",
+        "salary": "Rs. 1,200,000",
+        "location": "San Francisco, CA",
+        "description": "Analyze data to discover insights about Twitter users."
+    }
+]
+
 @app.route("/")
 def renderHome():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS, companyName="Stealth")
 
 
 if __name__ == "__main__":
